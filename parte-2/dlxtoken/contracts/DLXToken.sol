2cc146121a05648a1c9f78f74c2e9af97e149621
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-28 09:22:00 +0000

    fix constructor and mint for dkxtoken - parte 2

--- parte-2/dlxtoken/contracts/DLXToken.sol
+++ parte-2/dlxtoken/contracts/DLXToken.sol
@@ -14,11 +14,16 @@ contract DLXToken is ERC20, ERC20Detailed, Ownable {
      * DLX token constructor.
      * @param _participants list of participants' addresses
      */
-    constructor(address[] _participants)
+    constructor(address[] memory _participants)
         ERC20Detailed("DLXToken", "DLX", 18)
         public
     {
-        _mintForParticipants(1000);
+        // mint 1000 tokens to each participant
+        uint256 nUsers = _participants.length;
+        for (uint256 u = 0; u < nUsers; u += 1) {
+            participants.push(_participants[u]);
+            _mint(_participants[u], 1000);
+        }
         // make the multisig wallet the owner
         _transferOwnership(msg.sender);
     }
@@ -28,20 +33,10 @@ contract DLXToken is ERC20, ERC20Detailed, Ownable {
      * @param _amount amount to be minted for each participant
      */
     function mint(uint256 _amount) public onlyOwner {
-        _mintForParticipants(_amount);
-    }
-
-    /**
-     * private method to mint a given ammount
-     * of tokens to each participant.
-     * @param _amount amount to be minted for each participant
-     */
-    function _mintForParticipants(uint256 _amount) private {
         // mint _amount tokens to each participant
-        uint256 nUsers = _participants.length;
+        uint256 nUsers = participants.length;
         for (uint256 u = 0; u < nUsers; u += 1) {
-            participants.push(_participants);
-            _mint(_participants[u], _amount);
+            _mint(participants[u], _amount);
         }
     }
 }
