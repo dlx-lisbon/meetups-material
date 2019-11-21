0df764b75d8bcb7da3574c96f499e944718ccd8e
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-21 22:30:50 +0000

    add users accounts to deploy

--- parte-2/dlxtoken/migrations/2_deploy_contracts.js
+++ parte-2/dlxtoken/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const DLXToken = artifacts.require("DLXToken");
 
 module.exports = (deployer, network, accounts) => {
-    deployer.deploy(DLXToken, accounts);
+    deployer.deploy(DLXToken,
+        [
+            '0xA6b94Ce98D6CD4f447a9C6788F169DD17f65f747',
+            '0xB48577c8a13fE796573EA9636d985de189895b83',
+            '0xBd5E48c38D95770eEeA765f1A9b9C2C9d3244ed3',
+            '0xF5d349b0f35d8E224F356F3037FAECA0D39719FE',
+        ]);
 };
